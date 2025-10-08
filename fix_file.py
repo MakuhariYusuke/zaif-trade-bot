7b564a17c713073cad2030718529d19b514b5c89
幕張有祐 <ootkisk@gmail.com>
2025-10-09 02:08:35 +0900

    ルート直置きファイルの諸整理

--- fix_file.py
+++
@@ -1,8 +0,0 @@
-with open('ztb/tests/unit/training/test_paper_trade.py', 'r', encoding='utf-8') as f:
-    content = f.read()
-# 231行目までを保持
-lines = content.split('\n')
-clean_content = '\n'.join(lines[:232]) + '\n'
-with open('ztb/tests/unit/training/test_paper_trade.py', 'w', encoding='utf-8') as f:
-    f.write(clean_content)
-print('File fixed')
