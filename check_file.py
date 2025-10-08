423e94df2bf5233f3867810636148fe782437b18
幕張有祐 <ootkisk@gmail.com>
2025-10-09 02:08:35 +0900

    ルート直置きファイルの諸整理

--- check_file.py
+++
@@ -1,7 +0,0 @@
-import sys
-filename = sys.argv[1] if len(sys.argv) > 1 else 'ztb/tests/unit/training/test_unified_trainer.py'
-with open(filename, 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-print(f'Total lines: {len(lines)}')
-for i, line in enumerate(lines[-10:], len(lines)-9):
-    print(f'{i}: {repr(line)}')
--- clean_unified.py
+++
@@ -1,9 +0,0 @@
-with open('ztb/tests/unit/training/test_unified_trainer.py', 'r', encoding='utf-8') as f:
-    content = f.read()
-# </content> を含む行を削除
-lines = content.split('\n')
-clean_lines = [line for line in lines if '</content>' not in line and '<parameter name' not in line]
-clean_content = '\n'.join(clean_lines) + '\n'
-with open('ztb/tests/unit/training/test_unified_trainer.py', 'w', encoding='utf-8') as f:
-    f.write(clean_content)
-print('File cleaned')
