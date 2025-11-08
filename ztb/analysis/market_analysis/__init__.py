79f05fbfdd670b30207ed492e2618d00620ff2f1
幕張有祐 <ootkisk@gmail.com>
2025-11-09 05:06:31 +0900

    refactor: Eliminate code duplication in EnhancedRegimeAnalyzer
    - Remove EnhancedTechnicalIndicators class and integrate with existing feature generators
    - Use ztb.features.generators.technical modules for RSI, ADX, ATR, ROC, Bollinger Bands
    - Update __init__.py to remove deprecated imports and exports
    - Maintain all 12 market regime detection capabilities
    - Improve code maintainability and reduce duplication
    
    Addresses DRY principle violations and consolidates technical indicator calculations.

---
+++ ztb/analysis/market_analysis/__init__.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4d650a62140402bf0bfa2e310827afa93ef2461f3a7bf6c0259703ab965780cc
+size 709
---
+++ ztb/analysis/market_analysis/regime_analyzer.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:690456d42d588f6665366bacd44139ba9a9d8fe1b2e7f6d5ed02c49a061809be
+size 13587
