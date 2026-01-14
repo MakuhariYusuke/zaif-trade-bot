b06649fd2930cd688dfb87f73b209d5b6a18b1ce
幕張有祐 <ootkisk@gmail.com>
2026-01-14 20:46:07 +0900

    feat: Window分割の検証強化、Embargo実装、データリークチェック追加

--- ztb/evaluation/walk_forward/splitter.py
+++ ztb/evaluation/walk_forward/splitter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:591552499d52cd128087db24dd2145819921c0693cdc6e3ba15867fa02d3c8fe
-size 5973
+oid sha256:920fb72c1da14d310f0c1bc21c95fad477cd372d1b326bce8a89d5a18ff6eb9d
+size 10560
